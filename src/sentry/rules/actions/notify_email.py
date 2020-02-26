af655205d9e6bbaef492a0a80cb5e30172dc2e2a
jeffkwoh <23180853+jeffkwoh@users.noreply.github.com>
2020-02-26 14:05:01 -0800

    Amend form validation

--- src/sentry/rules/actions/notify_email.py
+++ src/sentry/rules/actions/notify_email.py
@@ -5,31 +5,31 @@ from __future__ import absolute_import
 
 from django import forms
 
-from sentry.plugins.base import plugins
+from sentry.db.models.fields.bounded import BoundedBigIntegerField
+
+# from sentry.plugins.base import plugins
 from sentry.rules.actions.base import EventAction
-from sentry.rules.actions.services import PluginService, SentryAppService
-from sentry.models import SentryApp
-from sentry.utils.safe import safe_execute
+
+# from sentry.rules.actions.services import PluginService, SentryAppService
+# from sentry.models import SentryApp
+# from sentry.utils.safe import safe_execute
 
 
-CHOICES = [["Members", "Members"], ["Owners", "Owners"]]
+CHOICES = [("Member", "Member"), ("Owner", "Owner"), ("Team", "Team")]
 
 
 class NotifyEmailForm(forms.Form):
-    service = forms.ChoiceField(choices=())
-
-    def __init__(self, *args, **kwargs):
-        self.fields["actors"].choices = CHOICES
-        self.fields["actors"].widget.choices = self.fields["actors"].choices
+    targetType = forms.ChoiceField(choices=CHOICES)
+    targetIdentifier = BoundedBigIntegerField().formfield(required=False)
 
 
 class NotifyEmailAction(EventAction):
     form_cls = NotifyEmailForm
-    label = "Send an email to {actors}"
+    label = "Send an email to {targetType}"
 
     def __init__(self, *args, **kwargs):
         super(NotifyEmailAction, self).__init__(*args, **kwargs)
-        self.form_fields = {"actors": {"type": "mailAction", "choices": CHOICES}}
+        self.form_fields = {"targetType": {"type": "mailAction", "choices": CHOICES}}
 
     def after(self, event, state):
         extra = {"event_id": event.event_id}
@@ -49,32 +49,32 @@ class NotifyEmailAction(EventAction):
         metrics.incr("notifications.sent", instance=plugin.slug, skip_internal=False)
         yield self.future(plugin.rule_notify)
 
-    def get_sentry_app_services(self):
-        apps = SentryApp.objects.filter(
-            installations__organization_id=self.project.organization_id, is_alertable=True
-        ).distinct()
-        results = [SentryAppService(app) for app in apps]
-        return results
+    # def get_sentry_app_services(self):
+    #     apps = SentryApp.objects.filter(
+    #         installations__organization_id=self.project.organization_id, is_alertable=True
+    #     ).distinct()
+    #     results = [SentryAppService(app) for app in apps]
+    #     return results
 
-    def get_plugins(self):
-        from sentry.plugins.bases.notify import NotificationPlugin
+    # def get_plugins(self):
+    #     from sentry.plugins.bases.notify import NotificationPlugin
+    #
+    #     results = []
+    #     for plugin in plugins.for_project(self.project, version=1):
+    #         if not isinstance(plugin, NotificationPlugin):
+    #             continue
+    #         results.append(PluginService(plugin))
+    #
+    #     for plugin in plugins.for_project(self.project, version=2):
+    #         for notifier in safe_execute(plugin.get_notifiers, _with_transaction=False) or ():
+    #             results.append(PluginService(notifier))
+    #
+    #     return results
 
-        results = []
-        for plugin in plugins.for_project(self.project, version=1):
-            if not isinstance(plugin, NotificationPlugin):
-                continue
-            results.append(PluginService(plugin))
-
-        for plugin in plugins.for_project(self.project, version=2):
-            for notifier in safe_execute(plugin.get_notifiers, _with_transaction=False) or ():
-                results.append(PluginService(notifier))
-
-        return results
-
-    def get_services(self):
-        services = self.get_plugins()
-        services += self.get_sentry_app_services()
-        return services
+    # def get_services(self):
+    #     services = self.get_plugins()
+    #     services += self.get_sentry_app_services()
+    #     return services
 
     def get_form_instance(self):
         return self.form_cls(self.data)
